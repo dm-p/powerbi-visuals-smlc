8269b266f2c010e9831e9d395d8e9386e2ad5b97
dm-p <daniel@coacervo.co>
2020-02-27 11:06:55 +1300

    #8: Fixed linting issues.

--- src/viewModel/IViewModel.ts
+++ src/viewModel/IViewModel.ts
@@ -1,31 +1,31 @@
 /** Power BI API Dependencies */
-    import powerbi from 'powerbi-visuals-api';
-    import IViewport = powerbi.IViewport;
-    import { legendInterfaces } from 'powerbi-visuals-utils-chartutils';
-    import ILegendData = legendInterfaces.LegendData;
+import powerbi from "powerbi-visuals-api";
+import IViewport = powerbi.IViewport;
+import { legendInterfaces } from "powerbi-visuals-utils-chartutils";
+import ILegendData = legendInterfaces.LegendData;
 
 /** Internal dependencies */
-    import ISmallMultiple from './ISmallMultiple';
-    import IStatistics from './IStatistics';
-    import IMeasure from './IMeasure';
-    import ICategory from './ICategory';
-    import ILayoutConfiguration from './ILayoutConfiguration';
-    import IAxis from './IAxis';
+import ISmallMultiple from "./ISmallMultiple";
+import IStatistics from "./IStatistics";
+import IMeasure from "./IMeasure";
+import ICategory from "./ICategory";
+import ILayoutConfiguration from "./ILayoutConfiguration";
+import IAxis from "./IAxis";
 
 /**
- *
+ * Manages necessary details for rendering the visual.
  */
-    export default interface IViewModel {
-        locale: string;
-        dataViewIsValid: boolean;
-        initialViewport: IViewport;
-        viewport: IViewport;
-        categoryMetadata: ICategory;
-        measureMetadata: IMeasure[];
-        multiples: ISmallMultiple[];
-        statistics: IStatistics;
-        layout: ILayoutConfiguration;
-        legend: ILegendData;
-        yAxis: IAxis;
-        xAxis: IAxis;
-    }
+export default interface IViewModel {
+  locale: string;
+  dataViewIsValid: boolean;
+  initialViewport: IViewport;
+  viewport: IViewport;
+  categoryMetadata: ICategory;
+  measureMetadata: IMeasure[];
+  multiples: ISmallMultiple[];
+  statistics: IStatistics;
+  layout: ILayoutConfiguration;
+  legend: ILegendData;
+  yAxis: IAxis;
+  xAxis: IAxis;
+}
